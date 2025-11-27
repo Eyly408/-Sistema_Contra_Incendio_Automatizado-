657e9ab18b14491bdd4a73913a25a9038fee569c
Luadvgvgvgvg <n00475858@upn.pe>
2025-11-27 16:17:22 -0500

    Add files via upload

---
+++ BibliotecaMonitoreo/BibliotecaMonitoreo/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaMonitoreo
+{
+    //Define la clase, los using, el namespace y crea el generador Random.
+    public class SistemaMonitoreo
+    {
+
+        Random rnd = new Random();
+
+    }
+}
