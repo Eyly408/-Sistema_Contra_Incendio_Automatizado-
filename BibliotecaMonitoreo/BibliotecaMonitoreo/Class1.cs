62a4f2e18bac1d9a805bd64f0a26efff5167827c
LADVHS08 <lg854218@gmail.com>
2025-11-27 16:36:42 -0500

    agregar método ObtenerTemperatura implementar cálculo del nivel de humo y limitar valores#1

--- BibliotecaMonitoreo/BibliotecaMonitoreo/Class1.cs
+++ BibliotecaMonitoreo/BibliotecaMonitoreo/Class1.cs
@@ -11,6 +11,28 @@ namespace BibliotecaMonitoreo
     {
 
         Random rnd = new Random();
+        public int ObtenerTemperatura()
+        {
+            int temperatura = rnd.Next(15, 81);
+            return temperatura;
+        }
+
+        public double ObtenerHumo(int temperatura)
+        {
+            double humo = (temperatura * 1.2) + rnd.Next(-10, 11);
+            
+            if (humo < 0)
+            {
+                humo = 0;
+            }
+
+            if(humo > 100)
+            {
+                humo = 100;
+            }
+
+            return Math.Round(humo, 1);
+        }
 
     }
 }
